2fa0982c4de9bd844f133ad565b4f41894368b62
yasir5405 <yasirnaseem5415@gmail.com>
2026-03-26 20:42:16 +0530

    Product section for mobile nav done

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../Logo";
 import {} from "../ui/navigation-menu";
 import { Button } from "../ui/button";
@@ -7,6 +7,94 @@ import { IconMenuDeep, IconX } from "@tabler/icons-react";
 import { InteractiveHoverButton } from "../ui/interactive-hover-button";
 import NavbarLinks from "./NavbarLinks";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import {
+  companySections,
+  productSections,
+  resourcesSections,
+  toolsSections,
+} from "@/constants/constants";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const MobileProductsAccordion = ({
+  section,
+}: {
+  section: {
+    title: string;
+    items: {
+      name: string;
+      description: string;
+      href: string;
+    }[];
+  }[];
+}) => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex w-full flex-col gap-4 font-heading">
+      <h1 className="text-muted-foreground text-sm">Product</h1>
+
+      <Accordion type="single" collapsible className="w-full">
+        {section.map((group, index) => (
+          <AccordionItem
+            key={index}
+            value={`item-${index}`}
+            className="border-none"
+          >
+            {/* Trigger */}
+            <AccordionTrigger className="text-2xl font-medium py-2 hover:no-underline flex items-center">
+              {group.title}
+            </AccordionTrigger>
+
+            {/* Content */}
+            <AccordionContent className="flex flex-col gap-0">
+              {group.items.map((item) => (
+                <h1
+                  key={item.name}
+                  onClick={() => navigate(item.href)}
+                  className="text-base text-white transition-colors px-2 py-2 rounded-md hover:bg-white/5 no-underline"
+                >
+                  {item.name}
+                </h1>
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
+};
+
+const MobileNavSections = ({
+  section,
+  title,
+}: {
+  section: {
+    title: string;
+    items: {
+      name: string;
+      description: string;
+      href: string;
+    }[];
+  }[];
+  title: string;
+}) => {
+  return (
+    <div className="flex w-full flex-col gap-4 font-heading">
+      <h1 className="text-muted-foreground text-sm">{title}</h1>
+
+      <div className="w-full flex flex-col gap-2.5">
+        {section.map((section) => (
+          <h1 className="font-medium text-2xl">{section.title}</h1>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -16,19 +104,19 @@ const Navbar = () => {
   }, [open]);
 
   return (
-    <nav className="fixed top-0 left-0 w-full h-16 border-b shadow-2xs flex items-center justify-between px-3 md:px-34 z-20 bg-background">
+    <nav className="-ml-1 fixed top-0 left-0 w-full h-16 border-b shadow-2xs flex items-center justify-between px-5 md:px-34 z-20 bg-background">
       {open && (
-        <div className="w-full fixed top-16 inset-0 z-50">
-          <div
-            className="fixed inset-0 top-16 bg-black/80 backdrop-blur-sm"
-            onClick={() => setOpen(false)}
-          ></div>
+        <div className="w-full fixed top-16 inset-0 py-8 z-50 bg-black/80 backdrop-blur-lg px-5 flex flex-col gap-10 overflow-y-auto">
+          <MobileProductsAccordion section={productSections} />
+          <MobileNavSections section={toolsSections} title="Tools" />
+          <MobileNavSections section={companySections} title="Company" />
+          <MobileNavSections section={resourcesSections} title="Resources" />
         </div>
       )}
 
       <Link
         to={"/"}
-        className="-ml-1 flex h-full w-fit items-center justify-center gap-1"
+        className="flex h-full w-fit items-center justify-center gap-1"
       >
         <Logo size="sm" />
 
@@ -49,15 +137,31 @@ const Navbar = () => {
           Get Started
         </InteractiveHoverButton>
 
-        {open ? (
-          <IconX onClick={() => setOpen(false)} />
-        ) : (
-          <IconMenuDeep
-            size={24}
-            className="block md:hidden lg:hidden xl:hidden"
-            onClick={() => setOpen(true)}
-          />
-        )}
+        <AnimatePresence mode="wait">
+          {open ? (
+            <motion.div
+              key="close"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.2 }}
+              className="block md:hidden"
+            >
+              <IconX onClick={() => setOpen(false)} />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="menu"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.2 }}
+              className="block md:hidden"
+            >
+              <IconMenuDeep onClick={() => setOpen(true)} />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </nav>
   );
